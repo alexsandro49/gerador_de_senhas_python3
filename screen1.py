93d2db7cb5dd17916474b80cd36a2c6d0b0379f2
Alex Sandro <alexsccpead@gmail.com>
2023-02-19 13:44:18 -0300

    Tela principal concluída

--- screen1.py
+++ screen1.py
@@ -9,9 +9,9 @@ main_screen.resize(500, 500)
 main_screen.setWindowTitle("Gerenciador de senhas")
 main_screen.setStyleSheet("background-color: #FF101F; color: #ffffff;")
 
-line_edit = QLineEdit("", main_screen)
-line_edit.setGeometry(50, 60, 400, 50)
-line_edit.setStyleSheet("background-color: #ffffff; color: #22333b; border: 2px solid #ffffff; border-radius: 5px; font-size: 18px; font-weight: bold; font-family: Arial, Verdana, sans-serif;")
+label1 = QLineEdit("", main_screen)
+label1.setGeometry(50, 60, 400, 50)
+label1.setStyleSheet("background-color: #ffffff; color: #22333b; border: 2px solid #ffffff; border-radius: 5px; font-size: 18px; font-weight: bold; font-family: Arial, Verdana, sans-serif;")
 
 checkbox1 = QCheckBox("Maiúsculas", main_screen)
 checkbox1.setGeometry(50, 130, 150, 30)
@@ -30,17 +30,23 @@ checkbox4.setGeometry(50, 280, 150, 30)
 checkbox4.setStyleSheet("background-color: #ffffff; color: #22333b; font-size: 12px; border: 2px solid #ffffff; border-radius: 5px; font-family: Arial, Verdana, sans-serif;")
 
 slider = QSlider(main_screen)
-slider.setGeometry(220, 130, 50, 180)
+slider.setGeometry(250, 130, 50, 180)
+slider.setRange(0, 50)
 slider.setStyleSheet("background-color: #ffffff; color: #22333b; font-size: 12px; border: 2px solid #ffffff; border-radius: 5px; font-family: Arial, Verdana, sans-serif;")
 
+label2 = QLabel("0", main_screen)
+label2.setGeometry(350, 130, 100, 40)
+label2.setStyleSheet("background-color: #22333b; border: 2px solid #ffffff; border-radius: 5px; font-size: 15px;")
+
 btn1 = QPushButton("GERAR", main_screen)
-btn1.setGeometry(320, 160, 100, 50)
+btn1.setGeometry(350, 200, 100, 40)
 btn1.setStyleSheet("background-color: #22333b; border: 2px solid #ffffff; border-radius: 5px;")
 
 btn2 = QPushButton("SALVAR", main_screen)
-btn2.setGeometry(320, 230, 100, 50)
+btn2.setGeometry(350, 270, 100, 40)
 btn2.setStyleSheet("background-color: #22333b; border: 2px solid #ffffff; border-radius: 5px;")
 
 btn3 = QPushButton("SENHAS SALVAS", main_screen)
-btn3.setGeometry(50, 340, 400, 50)
+btn3.setGeometry(50, 340, 400, 40)
 btn3.setStyleSheet("background-color: #22333b; border: 2px solid #ffffff; border-radius: 5px;")
+
